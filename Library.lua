061222a62682a145de40fb4fd178c8680b72260d
white558 <teramiss@gmail.com>
2026-07-13 13:59:54 +0300

    Added cornerelement to dropdowns

--- Library.lua
+++ Library.lua
@@ -5403,6 +5403,16 @@ do
                     Parent = Info.Multi and ButtonHolder or MenuTable.Menu,
                 })
 
+                if Library.CornerElements then
+                    table.insert(
+                        Library.Corners,
+                        New("UICorner", {
+                            CornerRadius = UDim.new(0, Library.CornerRadius / 2),
+                            Parent = Container,
+                        })
+                    )
+                end
+
                 local Image = ValueImage and New("ImageLabel", {
                     BackgroundTransparency = 1,
                     Image = ValueImage.Url,
